60abe7c6b6c6e231c6f399d1770a72de4701fcd8
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-03-16 13:45:53 +0530

    changing activity files to accommodate changes

---
+++ app/src/main/java/com/example/xpendy/CompareFragment.kt
@@ -0,0 +1,5 @@
+package com.example.xpendy
+
+import androidx.fragment.app.Fragment
+
+class CompareFragment:Fragment(R.layout.fragment_compare);
--- app/src/main/java/com/example/xpendy/MainActivity.kt
+++ app/src/main/java/com/example/xpendy/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.xpendy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.xpendy.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener{ item ->
+            when(item.itemId){
+                R.id.home -> replaceFragment(HomeFragment());
+                R.id.money -> replaceFragment(MoneyFragment());
+                R.id.overview -> replaceFragment(OverviewFragment());
+                R.id.compare -> replaceFragment(CompareFragment());
+                R.id.settings -> replaceFragment(SettingsFragment());
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager: FragmentManager = supportFragmentManager;
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
