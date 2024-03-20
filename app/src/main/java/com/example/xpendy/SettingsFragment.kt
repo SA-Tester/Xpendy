99f639ee42da71b9d4a51075f8ce233f7ee4e729
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2024-03-20 06:42:31 +0530

    Theme edited

--- app/src/main/java/com/example/xpendy/SettingsFragment.kt
+++ app/src/main/java/com/example/xpendy/SettingsFragment.kt
@@ -13,13 +13,11 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
         val switch: Switch = view.findViewById(R.id.switch2)
 
-        // Check current theme and set switch accordingly
         switch.isChecked = when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
             Configuration.UI_MODE_NIGHT_YES -> true
             else -> false
         }
 
-        // Set listener for switch to change theme
         switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
