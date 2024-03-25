74b97fbbf9ff683efacbbca70205d520a462cbc6
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-03-25 17:03:04 +0530

    Completing add income codes

--- app/src/main/java/com/example/xpendy/AddIncome.kt
+++ app/src/main/java/com/example/xpendy/AddIncome.kt
@@ -1,18 +1,15 @@
 package com.example.xpendy
 
-import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TextView
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Calendar
+import java.util.Locale
 
 class AddIncome : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +21,44 @@ class AddIncome : AppCompatActivity() {
             insets
         }
 
+        // Code for back button
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        toolbar.setNavigationOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.bottomNavigationView, MoneyFragment())
+                .commit()
+        }
+
+        val addIncomeBtn = findViewById<Button>(R.id.submitIncome)
+        addIncomeBtn.setOnClickListener{
+            val dbCursor = DBOperations()
+            val formats = Formats()
+
+            val note = getSource()
+            val amount = getAmount().toFloat()
+
+            var newData = "["
+            val incomesOfTheMonth = dbCursor.readIncome(this, formats.getCurrentMonth())
+            val dataList = formats.formatStringToArray(incomesOfTheMonth)
+            for ((key, value ) in dataList){
+                newData += "$key: $value,"
+            }
+            newData += "$note: $amount]"
+
+            dbCursor.removeIncome(this, formats.getCurrentMonth())
+            dbCursor.addIncome(this, formats.getCurrentMonth(), newData)
+        }
     }
 
+    private fun getSource(): String {
+        val noteInput = findViewById<EditText>(R.id.note_income)
+        return noteInput.text.toString()
+    }
 
+    private fun getAmount(): String{
+        val amountInput = findViewById<EditText>(R.id.income_amount)
+        return amountInput.text.toString()
+    }
 }
